5c71d2114073a0226deb9156710ac37993a68db3
NicolasLagaillardie <lagaillardie.nicolas@live.fr>
2018-10-23 15:40:24 +0200

    still working on week 2

--- Practical Time Series Analysis/week_2.R
+++ Practical Time Series Analysis/week_2.R
@@ -10,3 +10,29 @@ plot(globtemp, type ='o', main='Johnson&Johnson quaterly earnings per share', yl
 
 plot(star, type ='o', main='Johnson&Johnson quaterly earnings per share', ylab='Earnings', xlab='Years')
 
+#create a seed
+randomProcess = ts(rnorm(100))
+print(randomProcess)
+
+#compute autocovariance
+(acf(randomProcess, type='covariance'))
+
+
+acf(randomProcess, main='a Title')
+(acf(randomProcess, main='a Title'))
+
+data = rnorm(100,0,1)
+acf(2+3*x + data, main="ACF: Noise or Signal?")
+
+x=NULL
+x[1]=0
+for(i in 2:1000){
+  x[i] = x[i-1]+rnorm(1)
+}
+plot(x)
+
+randomWalk=ts(x)
+plot(randomWalk, main='Random walk')
+acf(randomWalk)
+plot(diff(randomWalk))
+acf(diff(randomWalk))
