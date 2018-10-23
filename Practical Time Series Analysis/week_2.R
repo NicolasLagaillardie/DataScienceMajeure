5564201342d89fd7559b0996363089aa7d61179e
NicolasLagaillardie <lagaillardie.nicolas@live.fr>
2018-10-23 10:35:20 +0200

    week 3

---
+++ Practical Time Series Analysis/week_2.R
@@ -0,0 +1,12 @@
+require(astsa)
+help("astsa")
+help(jj)
+
+plot(jj, type ='o', main='Johnson&Johnson quaterly earnings per share', ylab='Earnings', xlab='Years')
+
+plot(flu, type ='o', main='Johnson&Johnson quaterly earnings per share', ylab='Earnings', xlab='Years')
+
+plot(globtemp, type ='o', main='Johnson&Johnson quaterly earnings per share', ylab='Earnings', xlab='Years')
+
+plot(star, type ='o', main='Johnson&Johnson quaterly earnings per share', ylab='Earnings', xlab='Years')
+
